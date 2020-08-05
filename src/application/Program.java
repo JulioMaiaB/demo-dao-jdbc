22a8fe6d5d5d797ec6439cbad9f91e4469471a03
JulioMaiaB <julio_maia1@hotmail.com>
2020-08-04 21:21:55 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 		
 		Department dp = new Department(1, "Financeiro");
 		Seller sl = new Seller(12, "Julio", "julio@gmail.com", sdf.parse("20/03/1999"), 4000.0, dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(dp);
 		System.out.println(sl);
---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	// operacoes estaticas para instanciar os DAOs
+	
+	// macete para nao precisar expor a implementacao SellerDaoJDBC, pois retorna um SellerDao
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+}
